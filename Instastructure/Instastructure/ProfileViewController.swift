435464f9e0eac26bff9bbb30e3b8f6087cf63d8a
mariacaceres <airam923@hotmail.com>
2016-05-14 15:32:48 -0400

    updated ProfileViewController

--- Instastructure/Instastructure/ProfileViewController.swift
+++ Instastructure/Instastructure/ProfileViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 import Parse
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 320
+        tableView.reloadData()
+        
+        doQuery()
+        
+        let refreshControl = UIRefreshControl
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func doQuery() {
+        // construct PFQuery
+        let query = PFQuery(className: "Request")
+        query.orderByDescending("createdAt")
+        query.whereKey("author", equalTo: PFUser.currentUser()!)
+        query.limit = 20
+        
+        // fetch data asynchronously
+        query.findObjectsInBackgroundWithBlock { (media: [PFObject]?, error: NSError?) -> Void in
+            if let media = media {
+                self.requests = media
+                self.tableView.reloadData()
+            } else {
+                print(error?.localizedDescription)
+            }
+        }
     }
     
     @IBAction func onLogout(sender: AnyObject) {
